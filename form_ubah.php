c8ccb50733f0be77e86f9668f22a8190e15e9de6
aurelioklv <aurelioklv@gmail.com>
2023-05-21 19:06:18 +0700

    initial commit

---
+++ form_ubah.php
@@ -0,0 +1,59 @@
+<html>
+
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <title>Aplikasi CRUD dengan PHP</title>
+</head>
+
+<body>
+    <div class="container d-flex justify-content-center">
+        <div class="w-50">
+            <p class="h1 text-center mt-3">Ubah Data Siswa</p>
+            <?php
+            include "koneksi.php";
+
+            $id = $_GET['id'];
+
+            $sql = $pdo->prepare("SELECT * FROM siswa WHERE id=:id");
+            $sql->bindParam(':id', $id);
+            $sql->execute();
+
+            $data = $sql->fetch();
+            ?>
+            <form method="post" action="proses_ubah.php?id=<?php echo $id; ?>" enctype="multipart/form-data" class="mt-5">
+                <div class="mb-3">
+                    <label for="NIS" class="form-label">NIS</label>
+                    <input type="text" class="form-control" id="NIS" name="nis" value="<?php echo $data['nis']; ?>">
+                </div>
+                <div class="mb-3">
+                    <label for="name" class="form-label">Nama</label>
+                    <input type="text" class="form-control" id="name" name="nama" value="<?php echo $data['nama']; ?>">
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="male" value="Laki-laki" <?php echo ($data['jenis_kelamin'] == "Laki-laki") ? 'checked' : ''; ?> <label class="form-check-label" for="male">Laki-laki</label>
+                </div>
+                <div class="form-check form-check-inline">
+                    <input class="form-check-input" type="radio" name="jenis_kelamin" id="female" value="Perempuan" <?php echo ($data['jenis_kelamin'] == "Perempuan") ? 'checked' : ''; ?> <label class="form-check-label" for="female">Perempuan</label>
+                </div>
+                <div class="mb-3 mt-3">
+                    <label for="phone" class="form-label">Telepon</label>
+                    <input type="tel" class="form-control" id="phone" name="telp" value="<?php echo $data['telp']; ?>">
+                </div>
+                <div class="form-floating mb-3">
+                    <textarea class="form-control" placeholder="Your address" id="address" name="alamat"><?php echo $data['alamat']; ?></textarea>
+                    <label for="address">Alamat</label>
+                </div>
+                <div class="mb-3">
+                    <label for="photo" class="form-label">Foto</label>
+                    <input type="file" class="form-control" id="photo" name="foto">
+                </div>
+                <button type="submit" class="btn btn-primary">Ubah</button>
+                <a href="index.php" type="button" class="btn btn-secondary">Batal</a>
+            </form>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="container">
+        <p class="h1 text-center mt-3">Data Siswa</p>
+        <a href="form_simpan.php" class="btn btn-primary mt-5">Tambah Data</a>
+
+        <table class="table table-secondary table-striped table-hover mt-3">
+            <thead>
+                <tr>
+                    <th scope="col">Foto</th>
+                    <th scope="col">NIS</th>
+                    <th scope="col">Nama</th>
+                    <th scope="col">Jenis Kelamin</th>
+                    <th scope="col">Telepon</th>
+                    <th scope="col">Alamat</th>
+                    <th scope="col" colspan="2">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                include "koneksi.php";
+                $sql = $pdo->prepare("SELECT * FROM siswa");
+                $sql->execute();
+
+                while ($data = $sql->fetch()) {
+                    echo "<tr>";
+                    echo "<td><img src='images/" . $data['foto'] . "' width='100' height='100' class='img-thumbnail' alt='foto'></td>";
+                    echo "<td>" . $data['nis'] . "</td>";
+                    echo "<td>" . $data['nama'] . "</td>";
+                    echo "<td>" . $data['jenis_kelamin'] . "</td>";
+                    echo "<td>" . $data['telp'] . "</td>";
+                    echo "<td>" . $data['alamat'] . "</td>";
+                    echo "<td><a href='form_ubah.php?id=" . $data['id'] . "' class='btn btn-warning'>Ubah</a></td>";
+                    echo "<td><a href='proses_hapus.php?id=" . $data['id'] . "' class='btn btn-danger'>Hapus</a></td>";
+                    echo "</tr>";
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+</body>
+
+</html>
